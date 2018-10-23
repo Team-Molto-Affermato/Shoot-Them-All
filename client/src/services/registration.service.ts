16f06897542d08c219875eac98d076aa0299579b
Diego Pergolini <diego.pergolini@studio.unibo.it>
2018-10-23 11:55:30 +0200

    ROOOOOOOOOOOOOOOOOOOOOOOOOCK

--- client/src/services/registration.service.ts
+++ client/src/services/registration.service.ts
@@ -18,7 +18,7 @@ export class RegistrationService {
 
   register(user: User): Observable<any> {
 
-    return this.authenticationService.request('post', "/register", user)
+    return this.authenticationService.request('post', "/api/register", user)
       .pipe(
         catchError(this.errorsHandlerService.handleError)
       );
