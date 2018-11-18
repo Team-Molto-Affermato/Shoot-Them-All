baa828c344d40e8c4e8df851624c87dd3d39b8da
Luca Passeri <luca.passeri3@studio.unibo.it>
2018-11-18 10:31:17 +0100

    Merge branches 'master' and 'mistabuga' of https://github.com/Team-Molto-Affermato/Shoot-Them-All
    # Conflicts:
    #	client/src/app/app.module.ts

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,37 +1,45 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {HomeService} from "../../services/home.service";
 import {Match, MatchState} from "../../models/match";
 import {MatchInfoService} from "../../services/match-info.service";
 import {AuthenticationService} from "../../services/authentication.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {DataService} from '../../services/data.service';
 import {LocalStorageHelper, StorageKey} from "../../utilities/LocalStorageHelper";
 import {Subscription} from "rxjs";
+import {AbstractObserverComponent} from "../ObserverComponent";
+import {ConditionUpdaterService} from "../../services/condition-updater.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends AbstractObserverComponent implements OnInit, OnDestroy {
   showleaderboard = false;
   showMap =false;
   username;
   matches: Array<Match> = [];
   matchesSub: Subscription;
 
-  constructor(private router: Router,
-              private authenticationService: AuthenticationService,
+  constructor(private authenticationService: AuthenticationService,
               private homeService: HomeService,
-              private dataService: DataService) {
-    this.updateMatches();
+              private dataService: DataService,
+              router: Router,
+              conditionObserverService: ConditionUpdaterService,
+              route: ActivatedRoute) {
+    super(router, conditionObserverService, route);
   }
+
   switchComponent() {
     this.showleaderboard = !this.showleaderboard;
   }
+
   ngOnInit() {
+    this.init();
     this.username = LocalStorageHelper.getItem(StorageKey.USERNAME);
     this.dataService.sendMessage();
+    this.updateMatches();
     this.matchesSub = this.dataService.getMatches().subscribe(matches=>this.matches = matches);
   }
 
@@ -57,6 +65,10 @@ export class HomeComponent implements OnInit {
   userJoined(match: Match): boolean {
     return (match.state === MatchState.STARTED) &&
       match.users.includes(this.username)
+  }
+
+  ngOnDestroy(): void {
+    this.destroy();
   }
 
 }
